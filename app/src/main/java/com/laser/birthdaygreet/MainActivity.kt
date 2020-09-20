513cea633e9d8b02b498eb46a46b7b92a633e4cf
arjun-sudo <laserarjun@gmail.com>
2020-09-20 21:34:40 +0545

    Toast added

--- app/src/main/java/com/laser/birthdaygreet/MainActivity.kt
+++ app/src/main/java/com/laser/birthdaygreet/MainActivity.kt
@@ -2,11 +2,26 @@ package com.laser.birthdaygreet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        createBirthdayButton.setOnClickListener {
+//
+//        }
+
+
+    }
+    fun createBirthdayCard(view: View) {
+        val name=nameInput.editableText.toString()
+        Toast.makeText(this,"Name is $name",Toast.LENGTH_LONG).show()
+
     }
 }
+
 
